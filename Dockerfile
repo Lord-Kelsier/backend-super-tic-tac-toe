7a55e34676fa1033dc5d1b1cdba07c0bf0613a77
Lord-Kelsier <econtreraslazcano@uc.cl>
2024-03-28 01:01:46 -0300

    Added cors middleware, .env fle and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,12 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /App
 COPY . .
+EXPOSE 8000
 
 RUN apt-get update
 RUN apt-get -y install git
 RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN python -m pip install -r requirements.txt
-RUN ./manage.py migrate
-EXPOSE 8000
 
-CMD ["./manage.py", "runserver", "0.0.0.0:8000"]
+
+ENTRYPOINT ["/App/entrypoint.sh"]
--- backend/settings.py
+++ backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework'
 ]
 
@@ -52,6 +55,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -85,8 +89,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config["DB_NAME"],
+        'USER': config["DB_USER"],
+        'PASSWORD': config["DB_PASSWORD"],
+        'HOST': config["DB_HOST"],
+        'PORT': config["DB_PORT"],        
     }
 }
 
@@ -131,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = []
